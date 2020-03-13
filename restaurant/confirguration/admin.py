75a3f5297032329f19faa64539c3ce9848995ee5
blekou <mrblekou@gmail.com>
2020-03-13 17:04:06 +0000

    django resto

---
+++ restaurant/confirguration/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+admin.site.register(models.CompteSocial),
+admin.site.register(models.Site_info),
+admin.site.register(models.Presentation),
+admin.site.register(models.Temoignage)
